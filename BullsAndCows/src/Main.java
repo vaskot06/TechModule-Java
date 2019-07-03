b1995c31bedc8e4ef2af70ae30fb8be1a9ba2385
Vasil Tenev <vaskot06@gmail.com>
2019-07-03 23:10:16 +0300

    BullsAndCows game

---
+++ BullsAndCows/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        BullsAndCows app = new BullsAndCows();
+        app.createAppForm();
+
+        app.setVisible(true);
+    }
+
+}
